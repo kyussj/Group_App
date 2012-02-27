4476d98dce5bc6b9f83251f7d935c84614fef40d
Jack Booth <j.booth.07@aberdeen.ac.uk>
2012-02-27 15:01:38 +0000

    added a sign in and sign out function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 GroupApp::Application.routes.draw do
 resources :users
-  
+resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/signup',  to: 'users#new'
   match '/help',    to: 'group_app#help'
   match '/about',   to: 'group_app#about'
