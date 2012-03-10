03d6bb77170fa64ef24b02282b1209b30a30434f
Jack Booth <j.booth.07@aberdeen.ac.uk>
2012-03-10 17:08:31 +0000

    added user ability to delete posts

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,5 +1,6 @@
 class PostingsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: :destroy
 
   def index
   end
@@ -10,9 +11,21 @@ class PostingsController < ApplicationController
       flash[:success] = "Post created!"
       redirect_to root_path
     else
+	  @feed_items = []
       render 'group_app/home'
     end
   end
+  
   def destroy
+      @posting.destroy
+    redirect_back_or root_path
   end
+  
+    private
+
+    def correct_user
+      @posting = current_user.postings.find_by_id(params[:id])
+      redirect_to root_path if @posting.nil?
+    end
+	
 end
