bec4868e9ecde6f1ca6616047a9338c203d06c30
Jack Booth <j.booth.07@aberdeen.ac.uk>
2012-03-14 17:26:30 +0000

    edited readme and fixed some tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,16 +31,27 @@ describe "Authentication" do
 
 describe "Authorization" do
 
-    describe "for non-signed-in users" do
+ describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
       describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
 
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
-      end
+		
+	    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+	  end  
+     end
     end
   end
 end
